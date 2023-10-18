a72cf8cd51b4cc326458b839df72d94ccf755cdc
Carson Case <carsoncase@gmail.com>
2023-10-17 18:09:39 -0700

    first happy path test

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,35 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract KronosTest is Test {
+    Kronos public kronos;
 
+    address manager1 = 0x0001;
+    address manager2 = 0x0002;
+
+    address worker1 = 0x1000;
+    // set up the timeClock, deployer is the manager
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        vm.prank(manager1);
+        kronos = new Kronos();
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    // set up a new manager and make sure they have manager permissions
+    function testNewManager() public {
+        vm.prank(manager1);
+        kronos.setNewManager(manager2);
+        assertEq(kronos.isManager(manager2), true);
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    // a workday happy path of a worker clocking in and out again after 8 hours earning that many tokens
+    function workday() public {
+        vm.prank(worker1);
+        kronos.clockIn();
+        vm.warp(8 hours);
+        kronos.clockOut();
+
+        assertEq(kronos.balanceOf(worker1), 8 hours);
     }
+
 }
